fd61b03c076d1ed09b640abe1a80427cfb075740
Client John Subibi <alexvgamer2@gmail.com>
2022-06-22 23:17:55 +0800

    hey

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,10 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
@@ -11,6 +15,7 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -49,6 +54,36 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+ // instance not found
+ if ($exception instanceof ModelNotFoundException) {
+    $model = strtolower(class_basename($exception->getModel()));
+   
+    return $this->errorResponse("Does not exist any instance of
+   {$model} with the given id", Response::HTTP_NOT_FOUND);
+    }
+   
+    // validation exception
+    if ($exception instanceof ValidationException) {
+    $errors = $exception->validator->errors()->getMessages();
+    return $this->errorResponse($errors,Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+   
+   // access to forbidden
+    if ($exception instanceof AuthorizationException) {
+    return $this->errorResponse($exception->getMessage(),
+   Response::HTTP_FORBIDDEN);
+    }
+   
+    // unauthorized access
+    if ($exception instanceof AuthenticationException) {
+    return $this->errorResponse($exception->getMessage(),
+   Response::HTTP_UNAUTHORIZED);
+    }
+   
+    // if your are running in development environment
+    if (env('APP_DEBUG', false)) {
+    return parent::render($request, $exception);
+    }
+    return $this->errorResponse('Unexpected error. Try later', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
